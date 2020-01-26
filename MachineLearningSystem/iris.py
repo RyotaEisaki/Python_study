769db516f91636eca82d25f8ca1a8f1402287d34
MacBookPro <ryotaeisaki0530@icloud.com>
2020-01-26 17:45:36 +0900

    edit

---
+++ MachineLearningSystem/iris.py
@@ -0,0 +1,16 @@
+from matpotlib import pyplot as plt 
+from sklearn.datasets import load_iris
+
+data= load_iris()
+
+features = data['data']
+features_names=data['feature_names']
+target=data['target']
+target=names=data['target_name']
+labels=tarbet_name[target]
+
+for t,marker,c, in zip(xrange(3),">ox","rgb"):
+    plt.scatter(features[target==t,0],
+                features[target==t,1],
+                marker=marker,
+                c=c)
