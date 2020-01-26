3aee4e65469c6f9fc86354a2ae563d19caf33bf4
DellXPS <ryotaeisaki0530@icloud.com>
2020-01-26 18:15:57 +0900

    edit

--- MachineLearningSystem/iris.py
+++ MachineLearningSystem/iris.py
@@ -1,15 +1,16 @@
 from matplotlib import pyplot as plt 
 from sklearn.datasets import load_iris
+import numpy as np
 
 data= load_iris()
 
 features = data['data']
 features_names=data['feature_names']
 target=data['target']
-target=names=data['target_name']
-labels=tarbet_name[target]
+target_names=data['target_names']
+labels=target_names[target]
 
-for t,marker,c, in zip(xrange(3),">ox","rgb"):
+for t,marker,c in zip(range(3),">ox","rgb"):
     plt.scatter(features[target==t,0],
                 features[target==t,1],
                 marker=marker,
